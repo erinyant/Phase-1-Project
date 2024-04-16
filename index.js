79d2517fcf5590a90ddff634906ef5ce5164c69a
Erin Yant <ghosrider383@gmail.com>
2024-04-16 11:57:07 -0600

    new updates HTML, CSS, JS

---
+++ index.js
@@ -0,0 +1,62 @@
+fetch("http://localhost:3000/artwork")
+    .then((response) => response.json)
+    .then((data) => renderArt(data))
+
+function renderArt(objStoringUrls) {
+    urlArr = objStoringUrls.message
+
+    const imgContainer = document.querySelector('#art-image-container')
+
+    urlArr.forEach((url) => {
+        const img = document.createElement('img')
+        img.src = url
+        imgContainer.appendChild(img)
+    })
+}
+
+
+fetch("https://dog.ceo/api/breeds/list/all")
+    .then((resp) => resp.json())
+    .then((data) => renderArtNames(data))
+
+function renderBreedNames(objStoringBreeds) {
+    breedObj = objStoringBreeds.message
+    breedArr = Object.keys(breedObj)
+
+    const breedContainer = document.querySelector('#dog-breeds')
+
+    breedArr.forEach((breed) => {
+        const li = document.createElement('li')
+        li.textContent = breed
+        breedContainer.appendChild(li)
+
+        li.addEventListener('click', () => li.style.color = 'salmon')
+    })
+
+    const dropdown = document.querySelector('#breed-dropdown')
+
+    dropdown.addEventListener('change', (e) => handleSelectBreed(e))
+
+    function handleSelectBreed(e) {
+
+        breedContainer.textContent = ""
+
+        const filteredBreeds = breedArr.filter((breed) => {
+            if (breed.charAt(0) === e.target.value) {
+                return true
+            }
+            else {
+                return false
+            }
+            // return breed[0] === e.target.value
+        })
+
+        filteredBreeds.forEach((breed) => {
+            const li = document.createElement('li')
+            li.textContent = breed
+            breedContainer.appendChild(li)
+    
+            li.addEventListener('click', () => li.style.color = 'salmon')
+        })
+    }
+}
